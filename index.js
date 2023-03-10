9de6edf1b299a5c8ecc902da5fe19072d88c7c2e
JEdwards <jordanscottedwards@gmail.com>
2023-03-10 16:03:51 -0500

    Implemented error handling for creating new entries

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Name or number missing'
+        })
+    } else if (persons.find(person => body.name === person.name)) {
+        return response.status(400).json({
+            error: "Contact already in phonebook"
+        })
+    }
+    
     const newPerson = {
         "id": generateId(),
         "name": body.name,
